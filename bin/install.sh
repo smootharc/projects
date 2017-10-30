b7bf0a42d6b9efe6566e08d8a1cd89852f02d913
paul <paul>
2017-10-30 16:40:22 +0000

    Renamed bin/myinstall.sh to bin/install.sh.  Also added many missing packages to the apt install line.

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-tasksel install xubuntu-core
-tasksel install openssh-server
+#tasksel install xubuntu-core
+#tasksel install openssh-server
 #Latest mpv requires the following ppa.
 add-apt-repository ppa:mc3man/mpv-tests
 apt update
-apt install xfce4-goodies ranger mpv feh htop glances desktop-webmail firefox system-config-printer-gnome xfce4-pulseaudio-plugin pan sabnzbdplus zathura a2ps fossil dwww
+apt install \
+xfce4-goodies ranger mpv feh htop glances desktop-webmail \
+firefox system-config-printer-gnome xfce4-pulseaudio-plugin \
+pan sabnzbdplus zathura a2ps fossil dwww ncdu rlwrap smartmontools
+
 #dwww returns cgi errors fix whith sudo a2enmod cgi then sudo service apache2 restart
 pushd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
