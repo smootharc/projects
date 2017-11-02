f02b30e0ff55430903eaab8d0dd18fe17fd9d52b
paul <paul>
2017-11-02 03:57:19 +0000

    Added packages to bin/install.sh for tcl/tk and docs.  Also lines to fix the dwww cgi errors.

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 #tasksel install xubuntu-core
 #tasksel install openssh-server
 #Latest mpv requires the following ppa.
@@ -7,12 +8,16 @@ apt update
 apt install \
 xfce4-goodies ranger mpv feh htop glances desktop-webmail \
 firefox system-config-printer-gnome xfce4-pulseaudio-plugin \
-pan sabnzbdplus zathura a2ps fossil dwww ncdu rlwrap smartmontools backintime-qt4
+pan sabnzbdplus zathura a2ps fossil dwww ncdu rlwrap smartmontools backintime-qt4 \
+tcl8.6 tcl8.6-doc tk8.6 tk8.6-doc expect
 
 #dwww returns cgi errors fix whith sudo a2enmod cgi then sudo service apache2 restart
+a2enmod cgi
+service apache2 restart
+
 pushd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
+popd
 apt install -f
 apt autoremove
-popd
