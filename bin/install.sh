381aa73ff88fa580e3ad0aea203124acf4312fed
paul <paul>
2017-11-03 21:29:16 +0000

    Change #! line to bin/sh. in bin/weight, bin/extensions and bin/install.sh.  Added moreutils and lightdm-gtk-greeter-settings to bin/install.sh.

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-#tasksel install xubuntu-core
-#tasksel install openssh-server
 #Latest mpv requires the following ppa.
 add-apt-repository ppa:mc3man/mpv-tests
+
 apt update
+
 apt install \
-xfce4-goodies ranger mpv feh htop glances desktop-webmail \
+byobu xfce4-goodies ranger mpv feh htop glances desktop-webmail moreutils\
 firefox system-config-printer-gnome xfce4-pulseaudio-plugin \
 pan sabnzbdplus zathura a2ps fossil dwww ncdu rlwrap smartmontools backintime-qt4 \
-tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc expect
+tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc expect lightdm-gtk-greeter-settings
 
 #dwww returns cgi errors fix whith sudo a2enmod cgi then sudo service apache2 restart
 a2enmod cgi
