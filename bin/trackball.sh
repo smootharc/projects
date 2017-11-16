e4f76e8b1f3e23f5ed8ed62de0e2c5393dd0f0d3
paul <paul>
2017-11-16 13:05:59 +0000

    Removed bin/trackball.sh.  It's already in .xsessionrc.

--- bin/trackball.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#Use xinput list to determine the device number. The number immediately after set-prop is the device number.
-xinput set-prop 8 "Evdev Wheel Emulation" 1
-xinput set-prop 8 "Evdev Wheel Emulation Button" 8
