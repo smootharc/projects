b1ee44d14590c4c1a93cd9e5fa6ba470466c6349
paul <paul>
2017-11-25 05:49:19 +0000

    Removed libdatetime-perl.

--- bin/install.sh
+++ bin/install.sh
@@ -7,7 +7,7 @@ apt update
 
 apt install \
 byobu xfce4-goodies ranger mpv feh htop glances desktop-webmail moreutils \
-firefox system-config-printer-gnome xfce4-pulseaudio-plugin pinfo clementine libdatetime-perl \
+firefox system-config-printer-gnome xfce4-pulseaudio-plugin pinfo clementine \
 pan sabnzbdplus zathura a2ps fossil dwww ncdu rlwrap smartmontools backintime-qt4 sshfs \
 tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc expect lightdm-gtk-greeter-settings exim4
 
