5fea739cbd3650bbcba13f1fef80ba073a8f2e7c
paul <paul>
2017-12-04 20:45:09 +0000

    Added libsqlite3-tcl.

--- bin/install.sh
+++ bin/install.sh
@@ -9,7 +9,7 @@ apt install \
 byobu xfce4-goodies ranger mpv feh htop glances desktop-webmail moreutils \
 firefox system-config-printer-gnome xfce4-pulseaudio-plugin pinfo clementine \
 pan sabnzbdplus zathura a2ps fossil dwww ncdu rlwrap smartmontools backintime-qt4 sshfs \
-tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc tcl-tclreadline expect lightdm-gtk-greeter-settings exim4
+tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc tcl-tclreadline expect libsqlite3-tcl lightdm-gtk-greeter-settings exim4
 
 #dwww returns cgi errors fix whith sudo a2enmod cgi then sudo service apache2 restart.  Still need to edit a perl script to fix this completely.  I forget exactly what it is.
 a2enmod cgi
