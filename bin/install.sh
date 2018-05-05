fd67ac9f7dd5e6ae823edb43ac326b2d14e07491
paul <paul>
2018-05-05 23:14:41 +0000

    Added a2enmod cgi and systemctl restart apache2.

--- bin/install.sh
+++ bin/install.sh
@@ -14,7 +14,7 @@ add-apt-repository -y ppa:twodopeshaggy/jarun
 apt update
 
 apt-get -y install \
-tmux sakura ranger mpv feh glances moreutils configure-debian openssh-server apt-file xfce4-goodies moreutils \
+tmux sakura ranger mpv feh glances moreutils configure-debian openssh-server apt-file xfce4-goodies desktop-webmail \
 firefox pinfo hddtemp googler ddgr pdd python-pip vim-gtk herbstluftwm suckless-tools stalonetray restic byobu \
 dex zathura zathura-ps a2ps fossil dwww rlwrap smartmontools sshfs sqlite3 sqlite3-doc dict system-config-printer-gnome \
 tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc tcl-tclreadline expect libsqlite3-tcl exim4 megatools blueman zenity
