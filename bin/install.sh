ccf206ae90150c337493ced70b079b11b286eefc
paul <paul>
2018-05-13 13:18:16 +0000

    Excluded .debris files.

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd
+
+[ ! -d .local/bin ] && mkdir .local/bin
+
+[ ! -d bin ] && mkdir bin
+
 if ! [ $EUID = 0 ]
 then
     sudo "$0" "$@"
@@ -15,12 +21,14 @@ apt update
 
 apt-get -y install \
 tmux sakura ranger mpv feh glances moreutils configure-debian openssh-server apt-file xfce4-goodies desktop-webmail \
-firefox pinfo hddtemp googler ddgr pdd python-pip vim-gtk herbstluftwm suckless-tools stalonetray restic byobu \
+firefox pinfo hddtemp googler ddgr pdd python-pip vim-gtk herbstluftwm suckless-tools stalonetray restic byobu systemd-cron\
 dex zathura zathura-ps a2ps fossil dwww rlwrap smartmontools sshfs sqlite3 sqlite3-doc dict system-config-printer-gnome \
-tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc tcl-tclreadline expect libsqlite3-tcl exim4 megatools blueman zenity
+tcl8.6 tcllib tkinspect tcl8.6-doc tk8.6 tk8.6-doc tcl-tclreadline expect libsqlite3-tcl exim4 megatools blueman zenity wajig
 
 a2enmod cgi
 
 systemctl restart apache2
 
-echo "Remember sudo dpkg-reconfigure exim4-config."
+dpkg-reconfigure exim4-config
+
+echo "Install systemd-cron?"
