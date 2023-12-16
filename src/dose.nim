5525e38dd66ec27d28c455ae06e6d072a25e8966
Paul A. Medeiros <medeirospa@gmail.com>
2023-12-16 15:15:33 -0500

    Initial commit of src/dose.nim.

---
+++ src/dose.nim
@@ -0,0 +1,27 @@
+import std/os
+import std/parseopt
+import std/strutils
+import db_connector/db_sqlite
+import std/times
+import std/options
+import noise
+
+proc select() =
+  discard
+
+proc insert() =
+  discard
+
+proc update() =
+  discard
+
+proc delete() =
+  discard
+
+proc count() =
+  discard
+
+proc main() =
+  discard
+
+main()
