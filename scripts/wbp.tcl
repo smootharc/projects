36e04ad111ded9806645aa35f98b1b17cb77b536
Paul A. Medeiros <medeirospa@gmail.com>
2025-08-20 17:35:33 -0400

    Added headers and column formatting.

--- scripts/wbp.tcl
+++ scripts/wbp.tcl
@@ -4,8 +4,10 @@ package require sqlite3
 
 sqlite3 medical $env(HOME)/.local/share/medical.db -readonly true
 
-medical eval { select w.weight as weight, bp.datetime as datetime, bp.systolic as systolic, bp.diastolic as diastolic, bp.hr as hr
+puts [format {%s   %s                %s       %s     %s} LBS. DTM BP HR RPP]
+puts "----------------------------------------------"
+medical eval { select w.weight as weight, bp.datetime as datetime, bp.systolic as systolic, bp.diastolic as diastolic, bp.hr as hr, (bp.systolic * bp.hr) as rpp
                from weight as w
                join bp on w.date = date(bp.datetime)
                order by bp.datetime
- } { puts "$weight, $datetime, $systolic, $diastolic, $hr"}
+ } { puts [format {%.1f  %s  %7s %4d %8d} $weight $datetime $systolic/$diastolic $hr $rpp]}
