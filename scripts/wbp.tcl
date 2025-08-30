10fb63e407dc1800b93af0537989ac844cdde4d5
Paul A. Medeiros <medeirospa@gmail.com>
2025-08-30 17:06:03 -0400

    Add blood pressure comment to output.

--- scripts/wbp.tcl
+++ scripts/wbp.tcl
@@ -6,8 +6,8 @@ sqlite3 medical $env(HOME)/.local/share/medical.db -readonly true
 
 puts [format {%s   %s                %s       %s     %s} LBS. DTM BP HR RPP]
 puts "----------------------------------------------"
-medical eval { select w.weight as weight, bp.datetime as datetime, bp.systolic as systolic, bp.diastolic as diastolic, bp.hr as hr, (bp.systolic * bp.hr) as rpp
+medical eval { select w.weight as weight, bp.datetime as datetime, bp.systolic as systolic, bp.diastolic as diastolic, bp.hr as hr, (bp.systolic * bp.hr) as rpp, bp.comment as comment
                from weight as w
                join bp on w.date = date(bp.datetime)
                order by bp.datetime
- } { puts [format {%.1f  %s  %7s %4d %8d} $weight $datetime $systolic/$diastolic $hr $rpp]}
+ } { puts [format {%.1f  %s  %7s %4d %8d  %s} $weight $datetime $systolic/$diastolic $hr $rpp $comment]}
